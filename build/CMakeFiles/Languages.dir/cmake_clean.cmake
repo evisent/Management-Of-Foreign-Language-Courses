30590ad757a25948161265380055eb53b09c3b42
evisent <shlychkovdeni22s@gmail.com>
2025-10-13 17:00:35 +0300

    0.0.1

---
+++ build/CMakeFiles/Languages.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Languages.dir/src/languages.cpp.obj"
+  "CMakeFiles/Languages.dir/src/languages.cpp.obj.d"
+  "CMakeFiles/Languages.dir/src/main.cpp.obj"
+  "CMakeFiles/Languages.dir/src/main.cpp.obj.d"
+  "CMakeFiles/Languages.dir/src/student.cpp.obj"
+  "CMakeFiles/Languages.dir/src/student.cpp.obj.d"
+  "Languages.exe"
+  "Languages.exe.manifest"
+  "Languages.pdb"
+  "libLanguages.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Languages.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Languages.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Languages.
+# This may be replaced when dependencies are built.
